343dc21c42991e90ca0d105c6e1b84f956ec4035
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2026-05-13 11:30:00 +0600

    tallest added

---
+++ tallest.js
@@ -0,0 +1,13 @@
+ const hight = [ 68 , 69 , 70 , 71 , 72 , 73 , 74 , 75 , 76 , 77 , 78 , 79 , 80 ];
+
+ function getMax (numbers) {
+    let max = numbers[0];
+    for (number of numbers) {
+        
+        if (number > max) {
+            max = number;
+        }    }
+    return max;
+ }
+ const max = getMax(hight);
+ console.log("Maximum height:", max);
