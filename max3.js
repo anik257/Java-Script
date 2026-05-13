7fb2152ac2457646476d2e78a1830aabce24248a
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2026-05-13 10:03:35 +0600

    max3

---
+++ max3.js
@@ -0,0 +1,13 @@
+const Jim = 85;
+const Pam = 90;
+const Dwight = 92;
+
+if (Jim > Pam && Jim > Dwight) {
+    console.log("Jim has the highest score.");
+}
+else if (Pam > Jim && Pam > Dwight) {
+    console.log("Pam has the highest score.");
+}
+else {
+    console.log("Dwight has the highest score.");
+}
