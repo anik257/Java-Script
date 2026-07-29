75a7010bec5cb419f21d884a502d3448ecc8183d
Anik <ajaj.anik180@gmail.com>
2026-07-29 13:44:00 +0600

    added min price

---
+++ min.js
@@ -0,0 +1,12 @@
+const prices = [10, 20, 30, 40, 50];
+function getMin(numbers) {
+    let min = numbers[0];
+    for (const number of numbers) {
+        if (number < min) {
+            min = number;
+        }
+    }
+    return min;
+}
+const cheap = getMin(prices);
+console.log('The cheapest price is:', cheap);
